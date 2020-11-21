cc42214db80d7098bb29ca068a80e67a5c861f7f
IloveNooodles <mgarebaldhie80@gmail.com>
2020-11-21 20:49:57 +0700

    adding music

--- GIM 2.0 First Project/Player.gd
+++ GIM 2.0 First Project/Player.gd
@@ -7,6 +7,13 @@ var loncat = 1000
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func sfxjump():
+	var player = AudioStreamPlayer.new()
+	self.add_child(player)
+	player.stream = load("res://jump.wav")
+	player.play()
+	
+	
 func _physics_process(delta):
 	
 	arah.x = velocity
@@ -16,9 +23,14 @@ func _physics_process(delta):
 	arah.y += gravity * delta 
 	 
 	if Input.is_action_just_pressed("jump") and is_on_floor() :
+		sfxjump()
 		arah.y -= loncat
 	
 func _on_Area2D_body_entered(body):
 	if body.name == 'Player':
 		get_tree().change_scene("res://Main Scene.tscn")
 		
+
+
+func _on_Player_tree_entered():
+	pass # Replace with function body.
